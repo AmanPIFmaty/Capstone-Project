c37bd993f577ede350eea637e9eb0ab63a10a1e1
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-06 23:36:59 +0530

    Update output_guardrail.py

--- nodes/output_guardrail.py
+++ nodes/output_guardrail.py
@@ -15,24 +15,26 @@ def clean_abusive(text: str) -> str:
         text = text.replace(word, "***")
     return text
 
-
 @observe()
 def output_guard_node(state):
-    response = state["messages"][-1].content
+    response = state.get("response", "")
+
+    if not response:
+        response = "Sorry, I couldn't generate a response."
     response = clean_abusive(response)
     results = analyzer.analyze(
         text=response,
         entities=["EMAIL_ADDRESS", "PHONE_NUMBER"],
         language="en"
     )
+
     if results:
         response = anonymizer.anonymize(
             text=response,
             analyzer_results=results
         ).text
     return {
-        "messages":[
-            AIMessage(content=response)
-        ]
+        "messages": [AIMessage(content=response)]
     }
+
 
