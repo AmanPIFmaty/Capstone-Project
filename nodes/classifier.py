e421d0cfec5a85888a51baa6e812210de5801b60
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-06 12:35:59 +0530

    Update classifier.py

--- nodes/classifier.py
+++ nodes/classifier.py
@@ -2,15 +2,15 @@ import langgraph
 from services.state import ChatState
 from services.llm import llm
 from services.classifier import classify_query
+from langfuse import observe
 
 
+@observe()
 def classify_node(state: ChatState):
-    query = state["messages"][-1].content
-
+    query = state["query"]
     query_type = classify_query(query, llm)
 
     return {
         "query": query,
         "query_type": query_type
     }
-    
