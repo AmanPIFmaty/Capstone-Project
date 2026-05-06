b0a70db2547b1dc052742ae6ea14c44d771a130a
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-06 12:37:12 +0530

    Update retriever.py

--- nodes/retriever.py
+++ nodes/retriever.py
@@ -1,21 +1,24 @@
 import langgraph
+from langfuse import observe
 from services.state import ChatState
 from services.vectorstore import (
     mmr_retriever,
     get_hybrid_retriever
 )
 
-
+@observe()
 def retriever_node(state: ChatState):
     query = state["query"]
     topics = state.get("topics", [])
-
     if topics:
         docs = get_hybrid_retriever(topic=topics[0], k=5).invoke(query)
     else:
         docs = mmr_retriever.invoke(query)
+        
 
     return {
         "docs": docs
     }
+
+
 
