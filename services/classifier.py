21ee81cabce7f4b359c1d0ddae62998663ffd24e
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-06 12:42:55 +0530

    Update classifier.py

--- services/classifier.py
+++ services/classifier.py
@@ -1,17 +1,22 @@
 def classify_query(query, llm):
     prompt = f"""
-You are a query classifier.
+You are a strict query classifier.
 
-Return:
-- "rag" → for technical / IT / troubleshooting queries
-- "chat" → for casual conversation
-
-Return ONLY one word.
+Rules:
+- Return ONLY "rag" for technical / IT / troubleshooting queries
+- Return ONLY "chat" for casual conversation
+- Do NOT return anything 
+- Return ONLY one word.
+- RETURN "rag" if there is nothing in the query
 
 Query: {query}
 """
+
     response = llm.invoke(prompt).strip().lower()
 
-    if "rag" in response:
+    if response in ["rag", "technical", "tech", "it", "troubleshooting"]:
         return "rag"
+    elif response in ["chat", "general", "conversation"]:
+        return "chat"
+
     return "chat"
