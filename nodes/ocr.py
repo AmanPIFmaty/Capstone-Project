e14217d0b01d9dbc641410e6afe1fef959694e77
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-06 12:41:39 +0530

    Create ocr.py

---
+++ nodes/ocr.py
@@ -0,0 +1,14 @@
+from langchain_core.messages import HumanMessage
+from langfuse import observe
+from services.ocr import extract_text_from_bytes
+
+@observe()
+def ocr_node(state):
+    image_bytes = state["image_bytes"]
+    text = extract_text_from_bytes(image_bytes)
+
+    return {
+        "messages": [HumanMessage(content=text)], 
+        "query": text,
+        "image_bytes": None
+    }
