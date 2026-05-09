01642cc5293a445f6531c3f47ed1e97b5ec6f488
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-09 17:43:48 +0530

    Update topic.py

--- nodes/topic.py
+++ nodes/topic.py
@@ -1,16 +1,10 @@
+from services.topic_detector import detect_topic_from_query, get_topic_list
 import langgraph
 from services.state import ChatState
 from langfuse import observe
-from services.lazy_llm import llm
-from services.topic_detector import detect_topic_from_query, topic_list
-
 
 @observe()
 def topic_node(state: ChatState):
     query = state.get("final_query")
-
-    topics = detect_topic_from_query(query, topic_list, llm)
-
-    return {
-        "topics": topics
-    }
+    topics = detect_topic_from_query(query, get_topic_list())
+    return {"topics": topics}
