392ed1925aa4bd4c45d05ffec13e9ed34fe99a4f
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-09 17:38:04 +0530

    Update classifier.py

--- nodes/classifier.py
+++ nodes/classifier.py
@@ -1,6 +1,6 @@
 import langgraph
 from services.state import ChatState
-from services.llm import llm
+from services.llm import llm,qwenn_llm
 from services.classifier import classify_query
 from langfuse import observe
 
@@ -8,11 +8,14 @@ from langfuse import observe
 @observe()
 def classify_node(state: ChatState):
     query = state.get("final_query")
-    query_type = classify_query(query, llm)
+    query_type = classify_query(query, qwenn_llm)
 
     return {
         "final_query": query,
         "query_type": query_type
     }
+
+
+
 
 
