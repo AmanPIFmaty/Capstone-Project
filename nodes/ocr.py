ae699cd59191cce66a1e65e8be8f3e9fbd093f7a
AmanPIFmaty <shreejeealka79@gmail.com>
2026-05-10 13:32:45 +0530

    Update ocr.py

--- nodes/ocr.py
+++ nodes/ocr.py
@@ -5,8 +5,7 @@ from services.ocr import extract_text_from_bytes
 @observe()
 def ocr_node(state):
     image_bytes = state["image_bytes"]
-    #text = extract_text_from_bytes(image_bytes)
-    text="testing"
+    text = extract_text_from_bytes(image_bytes)
     return {
         #"messages": [HumanMessage(content=text)], 
         "ocr_text": text,
